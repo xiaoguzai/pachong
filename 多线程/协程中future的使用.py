3bdbf2ae4b2c3147809e7c4406d7937f80141ca7
xiaoguzai <31726901+boss2020@users.noreply.github.com>
2021-01-15 22:05:52 +0800

    Create 协程中future的使用.py

---
+++ 多线程/协程中future的使用.py
@@ -0,0 +1,16 @@
+import asyncio
+
+async def request(url):
+    print('正在请求的url是',url)
+    print('请求成功,',url)
+    return url
+#async修饰的函数，调用之后返回的一个协程对象
+#该函数如果被调用之后，函数内部的语句不会马上被执行
+c = request('www.baidu.com')
+
+#future的使用
+loop = asyncio.get_event_loop()
+task = asyncio.ensure_future(c)
+print(task)
+loop.run_until_complete(task)
+print(task)
